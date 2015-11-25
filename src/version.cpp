78bb4538c490fafaa8d91da8357c60578ad914d6
dankan1890 <mewuidev2@gmail.com>
2015-11-25 09:51:48 +0100

    Release 1.68

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 
 ***************************************************************************/
 
-#define BARE_BUILD_VERSION "0.167"
+#define BARE_BUILD_VERSION "0.168"
 
 extern const char bare_build_version[];
 extern const char build_version[];
@@ -17,4 +17,4 @@ const char build_version[] = BARE_BUILD_VERSION " (" __DATE__")";
 
 // MEWUI version
 extern const char mewui_version[];
-const char mewui_version[] = "1.67 r33";
+const char mewui_version[] = "1.68";
